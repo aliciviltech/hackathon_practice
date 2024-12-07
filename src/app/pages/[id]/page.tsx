d2be564f0c42780359614d5988eff76e08b8940b
aliciviltech <ali.civiltech@gmail.com>
2024-12-07 22:11:27 +0500

    second commit

---
+++ src/app/pages/[id]/page.tsx
@@ -0,0 +1,52 @@
+'use client'
+import { productsData } from '@/utils/productsData'
+import { useParams } from 'next/navigation'
+import React, { useEffect, useState } from 'react'
+
+interface ProductType {
+    id: number,
+    title: string,
+    imageURL: string
+}
+const page = () => {
+    const [targetProduct, setTargetProduct] = useState<ProductType>();
+    const param = useParams()
+    const { id } = param;
+
+    useEffect(() => {
+        const renderProduct = () => {
+            const selectedProduct = productsData.find((product, index) => {
+                return product.id === Number(id);
+            })
+            setTargetProduct(selectedProduct)
+        }
+        renderProduct()
+    }, [])
+    return (
+        <div>
+            <h1>Detailed Page</h1>
+            {/* {
+                productsData.map((product, index) => {
+                    return (
+                        product.id === Number(id) &&
+                        <div className="productDetails">
+                            <div className="image"><img src={product.imageURL} alt="" /></div>
+                            <div className="title">{product.title}</div>
+                        </div>
+                    )
+                })
+            } */}
+
+            {
+                targetProduct &&
+                <div className="productDetails">
+                    <div className="image"><img src={targetProduct.imageURL} alt="" /></div>
+                    <div className="title">{targetProduct.title}</div>
+                </div>
+            }
+
+        </div>
+    )
+}
+
+export default page
---
+++ src/utils/productsData.ts
@@ -0,0 +1,22 @@
+export const productsData = [
+    {
+        id:1,
+        title:'Lenovo X-265',
+        imageURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQu0R3bo60qkjNADtSJ1LtpNP-hCHJoHzGjkw&s'
+    },
+    {
+        id:2,
+        title:'Dell X-265',
+        imageURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaSkMFeWQP8Tq0V7m4wEjAIB9B3S4Wd1cjZA&s'
+    },
+    {
+        id:3,
+        title:'HP X-265',
+        imageURL:'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqC9omt0knL4hcC8ULP3HpLZTaULRfBp087w&s'
+    },
+    {
+        id:4,
+        title:'ASUS X-265',
+        imageURL:'https://media.istockphoto.com/id/2152858376/photo/asus-laptop-14-f415jf-ek156t.jpg?s=612x612&w=0&k=20&c=Xc2Slky2AoKvyr4R6VA7y5Smu5hYwH9HjE000CgTyNs='
+    }
+]
